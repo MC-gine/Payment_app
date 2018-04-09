78d5499e1de9194e20888359fb56855ffba5fad1
mc-gine <pestik_st@mail.ru>
2018-04-09 16:42:26 +0300

    Something

---
+++ SimplePaymentApp/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using GR.Net.Maroulis.Library;
+using System.Drawing;
+using Android.Views;
+using System;
+
+
+namespace SimplePaymentApp
+{
+    [Activity(Label = "SimplePaymentApp", MainLauncher = true, Icon ="@drawable/Icon", Theme ="@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //Show splash screen
+            var config = new EasySplashScreen(this)
+                .WithFullScreen()
+                .WithTargetActivity(Java.Lang.Class.FromType(typeof(TargetActivity)))
+                .WithSplashTimeOut(5000)
+                //.WithBackgroundResource(android.R.color.holo_red_light)
+                .WithLogo(Resource.Drawable.Logo)
+                .WithHeaderText("Wellcome to Simple Payment App")
+                .WithFooterText("copyright st.d.i 2018");
+
+
+            //Set Color for text
+            //config.getHeaderTextView().setTextColor(Color.White);
+            //config.getFooterTextView().setTextColor(Color.White);
+
+            //Create View
+            View view = config.Create();
+            SetContentView(view);
+        }
+    }
+}
+
---
+++ SimplePaymentApp/TargetActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Com.Craftman.Cardform;
+
+namespace SimplePaymentApp
+{
+    [Activity(Label = "TargetActivity", Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class TargetActivity : AppCompatActivity, IOnPayBtnClickListner
+    {
+        public void OnClick(Card card)
+        {
+            //If everything is correct
+            Toast.MakeText(ApplicationContext, "Payment was successful", ToastLength.Short).Show();
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Target);
+
+
+            var cardForm = FindViewById<CardForm>(Resource.Id.card_form);
+            var txt_amount = FindViewById<TextView>(Resource.Id.payment_amount);
+            var btnPay = FindViewById<Button>(Resource.Id.btn_pay);
+
+
+            txt_amount.Text = "1 Euro";
+            btnPay.Text = $"Payer {txt_amount.Text}";
+
+            cardForm.SetPayBtnClickListner(this);
+        }
+    }
+}
